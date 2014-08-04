11d09c4a63686ac6e5825a905f1452c52f8b6d2f
Alan Rodrigo da Silva Marcon <alanrodrigo@outlook.com.br>
2014-08-04 16:09:18 -0400

    Create using-scipy-multi.py
    on linux terminal:
    user@user:~$ python using-scipy-multi.py <filename.wav>

---
+++ stereotomono/using-scipy-multi.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+#Alan Rodrigo
+#UFMT -IC
+#INAU
+
+from scipy.io import wavfile
+import glob
+import os
+arquivos = glob.glob('*.wav')
+
+os.system("clear")
+
+for i in arquivos:
+	fs, data = wavfile.read(arquivos[i])
+	wavfile.write(audio_file[:-4]+'-l.wav',fs,data[:,0])
+	wavfile.write(audio_file[:-4]+'-r.wav',fs,data[:,1])
+	wavfile.closeall()
